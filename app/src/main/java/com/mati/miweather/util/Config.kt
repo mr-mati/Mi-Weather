81c408cbc15239c08ddcd9d06528940da85c8d24
Mahdi Mohammadi <mahdi60mm60@gmail.com>
2023-10-29 17:42:55 +0330

    Add Api Key

--- app/src/main/java/com/mati/miweather/util/Config.kt
+++ app/src/main/java/com/mati/miweather/util/Config.kt
@@ -1,5 +1,5 @@
 package com.mati.miweather.util
 
 const val BASE_URL = "https://api.openweathermap.org/"
-
+const val API_KEY = "489c59265fb912e0096ba30d3fa6e79f"
 const val BASE_IMAGE_URL = "https://openweathermap.org/img/wn/"
