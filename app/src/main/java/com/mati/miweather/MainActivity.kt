7d6d02c00d9b52fe921cffad39a46cfc80b3c947
Mahdi Mohammadi <mahdi60mm60@gmail.com>
2023-10-31 14:43:54 +0330

    Save

--- app/src/main/java/com/mati/miweather/MainActivity.kt
+++ app/src/main/java/com/mati/miweather/MainActivity.kt
@@ -34,7 +34,9 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Navigation(viewModel)
+
+                        Navigation(viewModel)
+
                 }
             }
         }
--- app/src/main/java/com/mati/miweather/ui/navigate/NavigationItems.kt
+++ app/src/main/java/com/mati/miweather/ui/navigate/NavigationItems.kt
@@ -1,6 +1,6 @@
 package com.mati.mimovies.utils
 
-sealed class NavigationItem(val route:String){
+sealed class NavigationItem(val route: String) {
 
     object MainScreen : NavigationItem("mainScreen")
 
