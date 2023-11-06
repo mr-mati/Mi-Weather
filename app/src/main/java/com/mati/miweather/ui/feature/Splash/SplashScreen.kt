c3bcd5f98cf155b93e624b306f6ef968ab0e8db4
Mahdi Mohammadi <mahdi60mm60@gmail.com>
2023-11-07 00:59:49 +0330

    Splash Screen Ui

--- app/src/main/java/com/mati/miweather/ui/feature/Splash/SplashScreen.kt
+++ app/src/main/java/com/mati/miweather/ui/feature/Splash/SplashScreen.kt
@@ -1,27 +1,30 @@
 package com.mati.miweather.ui.feature.Splash
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.BlurredEdgeTreatment
-import androidx.compose.ui.draw.blur
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
-import coil.compose.rememberAsyncImagePainter
-import coil.request.ImageRequest
+import com.airbnb.lottie.compose.LottieAnimation
+import com.airbnb.lottie.compose.LottieCompositionSpec
+import com.airbnb.lottie.compose.LottieConstants
+import com.airbnb.lottie.compose.rememberLottieComposition
+import com.mati.miweather.R
 import com.mati.miweather.ui.theme.Background
 import com.mati.miweather.ui.theme.Background1
+import com.mati.miweather.util.DataTime
 
 @Composable
 fun SplashScreen() {
@@ -33,32 +36,55 @@ fun SplashScreen() {
             .fillMaxSize()
             .background(brush = backgroundColor)
     ) {
-        Image(
-            rememberAsyncImagePainter(
-                model = ImageRequest.Builder(LocalContext.current)
-                    .data("https://cutewallpaper.org/23/sky-night-wallpaper/2142210133.jpg")
-                    .build()
-            ),
+        val composition by rememberLottieComposition(
+            LottieCompositionSpec.RawRes(
+                loaderAnim()
+            )
+        )
+        LottieAnimation(
             modifier = Modifier
-                .fillMaxSize()
-                .blur(
-                    radiusX = 20.dp,
-                    radiusY = 20.dp,
-                    edgeTreatment = BlurredEdgeTreatment(RoundedCornerShape(16.dp))
-                ),
-            contentScale = ContentScale.FillBounds,
-            contentDescription = ""
+                .size(270.dp, 270.dp)
+                .align(Alignment.Center)
+                .background(Color.Transparent),
+            composition = composition,
+            alignment = Alignment.Center,
+            iterations = LottieConstants.IterateForever,
         )
         Column(
             modifier = Modifier
                 .fillMaxSize(),
-            verticalArrangement = Arrangement.Center,
+            verticalArrangement = Arrangement.Bottom,
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            CircularProgressIndicator(
-                color = Color.Blue, // You can set the color of the CircularProgressIndicator
-                strokeWidth = 5.dp, // You can adjust the stroke width
+            val anim by rememberLottieComposition(
+                LottieCompositionSpec.RawRes(
+                    R.raw.loader
+                )
+            )
+            LottieAnimation(
+                modifier = Modifier
+                    .size(100.dp, 80.dp)
+                    .padding(bottom = 32.dp)
+                    .background(Color.Transparent),
+                composition = anim,
+                iterations = LottieConstants.IterateForever,
             )
         }
     }
+}
+
+@Composable
+fun loaderAnim(): Int {
+    var anim by remember { mutableStateOf(R.raw.day) }
+    val time = DataTime.getHour()
+    when (time) {
+        in 5..18 -> {
+            anim = R.raw.loader_day
+        }
+
+        else -> {
+            anim = R.raw.loader_night
+        }
+    }
+    return anim
 }
