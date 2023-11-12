c6c49c22a4d2c41970d68fcd4dfeb59509d21680
Mahdi Mohammadi <mahdi60mm60@gmail.com>
2023-11-12 12:08:55 +0330

    Fix City Name | Add New String

--- app/src/main/java/com/mati/miweather/ui/feature/SelectCity/SelectCity.kt
+++ app/src/main/java/com/mati/miweather/ui/feature/SelectCity/SelectCity.kt
@@ -4,19 +4,23 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
@@ -31,6 +35,7 @@ import com.mati.miweather.ui.theme.Black
 import com.mati.miweather.ui.theme.Primary
 import com.mati.miweather.ui.theme.Transparent
 import com.mati.miweather.ui.theme.White
+import com.mati.miweather.ui.theme.onPrimary
 
 @Composable
 fun SelectCity(viewModel: MainViewModel, visible: () -> Unit) {
@@ -85,15 +90,31 @@ fun SelectCity(viewModel: MainViewModel, visible: () -> Unit) {
             modifier = Modifier
                 .background(brush = backgroundColor),
         ) {
-            Text(
+            Row(
                 modifier = Modifier
                     .padding(start = 16.dp, top = 8.dp, bottom = 4.dp)
                     .align(Alignment.Start),
-                fontWeight = FontWeight.Bold,
-                text = stringResource(R.string.city_selected), style = TextStyle(
-                    color = Primary, fontSize = 24.sp
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Icon(
+                    modifier = Modifier
+                        .size(48.dp)
+                        .clickable {
+                            visible()
+                        },
+                    painter = painterResource(id = R.drawable.back),
+                    tint = onPrimary,
+                    contentDescription = ""
                 )
-            )
+                Text(
+                    modifier = Modifier
+                        .padding(start = 4.dp),
+                    fontWeight = FontWeight.Bold,
+                    text = stringResource(R.string.city_selected), style = TextStyle(
+                        color = Primary, fontSize = 24.sp
+                    )
+                )
+            }
             LazyColumn(
                 modifier = Modifier
                     .padding(top = 16.dp, bottom = 16.dp)
